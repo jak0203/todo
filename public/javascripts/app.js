4b5eb6e67653d971cafb0c01c331c7c2e5989e57
Danny Lynch <danny31415@gmail.com>
2015-08-29 15:37:09 -0700

    remove unneeded (broken) calls

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -40,26 +40,3 @@ angular.module('nodeTodo', [])
             });
     };
 });
-
-// Create a new todo
-$http.post('/api/v1/todos', $scope.formData)
-    .success(function(data) {
-        $scope.formData = {};
-        $scope.todoData = data;
-        console.log(data);
-    })
-    .error(function(error) {
-        console.log('Error: ' + error);
-    });
-
-// Delete a todo
-$http.delete('/api/v1/todos/' + todoID)
-    .success(function(data) {
-        $scope.todoData = data;
-        console.log(data);
-    })
-    .error(function(data) {
-        console.log('Error: ' + data);
-    });
-
-
