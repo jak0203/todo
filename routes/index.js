e3c3a62010bd761c15901eaecf75653cf6483571
Jacki Lynch <jak0203@gmail.com>
2015-08-29 14:01:41 -0700

    make this mor pretty

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 router.get('/api/v1/todos', function(req, res) {
